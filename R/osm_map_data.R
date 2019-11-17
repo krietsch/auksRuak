e08f322d435f041c15eec6a1523f9b1c50a79b00
krietsch <jkrietsch@orn.mpg.de>
2019-11-17 15:41:44 +0100

    simplified osm description in one file

---
+++ R/osm_map_data.R
@@ -0,0 +1,125 @@
+
+#' Base map polygons
+#'
+#' Open street map polygons of land
+#'
+#' @name osm_land
+#'
+#' @docType data
+#'
+#' @usage data(osm_land)
+#'
+#' @format sf polygons from open street map  in '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' \itemize{
+#'   \item land
+#'   }
+#'
+#' @references \url{http://openstreetmapdata.com}
+#' @source \url{http://openstreetmapdata.com/data/land-polygons}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' osm_land
+#'
+NULL
+
+#' Base map polygons
+#'
+#' Open street map polygons of lakes
+#'
+#' @name osm_lakes
+#'
+#' @docType data
+#'
+#' @usage data(osm_lakes)
+#'
+#' @format sf polygons from open street map  in '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' \itemize{
+#'   \item lakes
+#'   }
+#'
+#' @references \url{http://openstreetmapdata.com}
+#' @source \url{http://download.geofabrik.de/north-america/us/alaska.html}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' osm_lakes
+#'
+NULL
+
+#' Base map polygons
+#'
+#' Open street map polygons of rivers
+#'
+#' @name osm_rivers
+#'
+#' @docType data
+#'
+#' @usage data(osm_rivers)
+#'
+#' @format sf polygons from open street map  in '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' \itemize{
+#'   \item rivers
+#'   }
+#'
+#' @references \url{http://openstreetmapdata.com}
+#' @source \url{http://download.geofabrik.de/north-america/us/alaska.html}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' osm_rivers
+#'
+NULL
+
+#' Base map polygons
+#'
+#' Open street map polygons of roads
+#'
+#' @name osm_roads
+#'
+#' @docType data
+#'
+#' @usage data(osm_roads)
+#'
+#' @format sf polygons from open street map  in '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' \itemize{
+#'   \item roads
+#'   }
+#'
+#' @references \url{http://openstreetmapdata.com}
+#' @source \url{http://download.geofabrik.de/north-america/us/alaska.html}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' osm_roads
+#'
+NULL
+
+#' Base map polygons
+#'
+#' Open street map polygons of buildings
+#'
+#' @name osm_buildings
+#'
+#' @docType data
+#'
+#' @usage data(osm_buildings)
+#'
+#' @format sf polygons from open street map  in '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
+#' \itemize{
+#'   \item buildings
+#'   }
+#'
+#' @references \url{http://openstreetmapdata.com}
+#' @source \url{http://openstreetmapdata.com/data/land-polygons}
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' osm_buildings
+#'
+NULL
