53c65fded44ae25d9d51e5ca56683a13790cb5d0
krietsch <jkrietsch@orn.mpg.de>
2019-11-18 11:09:07 +0100

    changed extent of the study site

--- inst/map_source/study_site.R
+++ inst/map_source/study_site.R
@@ -10,7 +10,7 @@ sapply( c('data.table', 'magrittr', 'sf'),
 PROJ = '+proj=laea +lat_0=90 +lon_0=-156.653428 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs +ellps=WGS84 +towgs84=0,0,0 '
 
 # study area
-file_path = system.file('map_source', 'study_site.kml', package = 'auksruak')
+file_path = system.file('map_source', 'study_site.kml', package = 'auksRuak')
 study_site = st_read(file_path, quiet  = TRUE) %>% st_geometry %>% st_zm(drop = TRUE)
 
 # change projection
