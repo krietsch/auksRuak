8551d7cac69b629669417230e1d046c66137b14f
krietsch <jkrietsch@orn.mpg.de>
2019-11-23 11:20:04 +0100

    added test for obs_id_check

--- tests/testthat/test-obs_id_check.R
+++ tests/testthat/test-obs_id_check.R
@@ -1,3 +1,11 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+context('obs_id_check')
+
+
+test_that('obs_id_check', {
+
+  x = obs_id_check('18_2_221')
+
+  expect_that( length(x), equals(7) )
+
 })
+
